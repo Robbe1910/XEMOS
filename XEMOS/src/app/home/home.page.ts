5da439f0d6e8f75c70a561261c48026627d191d5
Alvaromg00 <amorong03@informatica.iesvalledeljerteplasencia.es>
2024-04-19 18:39:17 +0200

    feat: :sparkles: line chart de humedad y estilos de las charts

--- XEMOS/src/app/home/home.page.ts
+++ XEMOS/src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import Chart from 'chart.js/auto';
+import Chart, { ChartConfiguration } from 'chart.js/auto';
 
 @Component({
   selector: 'app-home',
@@ -10,26 +10,25 @@ export class HomePage implements OnInit {
 
   chart: any;
   heartRate: number = 0; // Variable para almacenar el último valor de ritmo cardíaco
-  bpmGradient: string = 'linear-gradient(to right, green, yellow, orange, red, blue)'; // Gradiente de colores de la barra de BPM
+  bpmGradient: string = ''; // Gradiente de colores de la barra de BPM
   indicatorPosition: string = '0%'; // Posición del indicador de BPM en porcentaje
 
   constructor() {}
 
   ngOnInit() {
-    // Obtener la hora actual
-    const currentTime = new Date();
     
-    // Generar las etiquetas de tiempo para las últimas 24 horas
     const labels = [];
-    for (let i = 23; i >= 0; i--) {
-      const pastTime = new Date(currentTime);
-      pastTime.setHours(currentTime.getHours() - i);
+    const currentTime = new Date();
+    for (let i = 0; i < 24; i += 2) {
+      const pastTime = new Date(currentTime.getTime() - (i * 60 * 60 * 1000));
       const hour = pastTime.getHours();
-      labels.push(hour.toString());
+      labels.unshift(hour.toString());
     }
     
-    // Generar datos aleatorios de pulsaciones cardíacas
-    const heartRateData = this.generateRandomHeartRateData(labels.length);
+    const heartRateData = [];
+    for (let i = 0; i < 24 * 6; i++) { // 24 horas * 6 intervalos por hora
+      heartRateData.push(Math.floor(Math.random() * (100 - 60 + 1)) + 60); // Genera un dato aleatorio entre 60 y 100
+    }
   
     // Obtener el último valor de ritmo cardíaco
     this.heartRate = heartRateData[heartRateData.length - 1];
@@ -37,81 +36,169 @@ export class HomePage implements OnInit {
     // Actualizar el gradiente de colores y la posición del indicador de BPM
     this.updateBpmGradient();
   
-    // Crear el gráfico de líneas con las etiquetas generadas dinámicamente y los datos aleatorios de pulsaciones cardíacas
-    this.chart = new Chart('canvas', {
+    // Crear el gráfico de líneas con los datos generados
+    this.chart = new Chart('heart-rate', {
       type: 'line',
       data: {
         labels: labels,
         datasets: [{
-          label: 'Heart Rate',
+          label: 'BPM',
           data: heartRateData,
-          borderColor: 'red', // Cambiar el color de la línea a rojo
-          backgroundColor: 'rgba(255, 0, 0, 0.2)', // Cambiar el color de fondo del área bajo la línea a rojo semi-transparente
+          borderColor: '#DE1835 ',
+          backgroundColor: '#EE3E0B',
           borderWidth: 1
         }]
       },
       options: {
-        responsive: true // Añadimos la opción responsive
+        scales: {
+          y: {
+            min: 0,
+            max: 180,
+            title: {
+              display: true,
+              text: ''
+            }
+          }
+        }
       }
     });
+
+
+    const DATA_COUNT = 12; // Cantidad de datos
+const data = {
+  labels: this.generateLabels(DATA_COUNT), // Genera etiquetas para los datos
+  datasets: [{
+    data: this.generateData(DATA_COUNT) // Genera datos aleatorios para la humedad del aire
+  }]
+};
+
+const config: ChartConfiguration<'line'> = {
+  type: 'line', // Tipo de gráfico: línea
+  data: data, // Datos del gráfico
+  options: {
+    plugins: {
+      legend: {
+        display: false // Oculta la leyenda
+      },
+      tooltip: {
+        enabled: true // Activa el tooltip
+      },
+    },
+    elements: {
+      line: {
+        fill: false, // No rellena el área bajo la línea
+        backgroundColor: this.getLineColor.bind(this), // Color de fondo de la línea
+        borderColor: this.getLineColor.bind(this) // Color de borde de la línea
+      },
+      point: {
+        backgroundColor: this.getLineColor.bind(this), // Color de fondo del punto
+        hoverBackgroundColor: this.makeHalfAsOpaque.bind(this), // Color de fondo del punto al pasar el mouse
+        radius: this.adjustRadiusBasedOnData.bind(this), // Ajusta el radio del punto basado en los datos
+        pointStyle: this.alternatePointStyles.bind(this), // Alterna los estilos de los puntos
+        hoverRadius: 15, // Radio del punto al pasar el mouse
+      }
+    }
+  }
+};
+
+this.chart = new Chart('air-quality', config);
+  }
+
+  generateLabels(count: number): string[] {
+    const labels = [];
+    for (let i = 1; i <= count; i++) {
+      labels.push(`Hora ${i}`); // Etiquetas de las horas
+    }
+    return labels;
   }
   
-  generateRandomHeartRateData(numPoints: number): number[] {
-    const heartRateData = [];
-    for (let i = 0; i < numPoints; i++) {
-      const heartRate = Math.floor(Math.random() * (150 - 60 + 1)) + 60; // Genera un número aleatorio entre 60 y 150
-      heartRateData.push(heartRate);
+  generateData(count: number): number[] {
+    const data = [];
+    for (let i = 0; i < count; i++) {
+      data.push(Math.floor(Math.random() * 100)); // Genera números aleatorios entre 0 y 100 para la humedad
     }
-    return heartRateData;
+    return data;
   }
-
-  updateBpmGradient() {
-    // Calcula la posición del indicador de BPM en porcentaje
-    const positionPercentage = (this.heartRate - 60) / (150 - 60) * 100;
-    this.indicatorPosition = `${positionPercentage}%`;
-
-    // Actualiza el gradiente de colores de la barra de BPM
-    this.bpmGradient = 'linear-gradient(to right, ' +
-      this.getBpmColor(60) + ' 0%, ' +
-      this.getBpmColor(81) + ' 25%, ' +
-      this.getBpmColor(101) + ' 50%, ' +
-      this.getBpmColor(121) + ' 75%, ' +
-      this.getBpmColor(150) + ' 100%)';
+  
+  getLineColor(ctx: any) {
+    // Verifica si ctx.parsed está definido y tiene una propiedad 'y'
+    if (ctx.parsed && ctx.parsed.y !== undefined) {
+      // Devuelve un color basado en el valor de la humedad
+      const humidity = ctx.parsed.y;
+      if (humidity < 30) {
+        return 'rgba(75, 192, 192, 1)'; // Azul para baja humedad
+      } else if (humidity < 60) {
+        return 'rgba(255, 193, 7, 1)'; // Amarillo para humedad media
+      } else {
+        return 'rgba(255, 105, 97, 1)'; // Rojo para alta humedad
+      }
+    } else {
+      // Manejo del caso en que ctx.parsed o ctx.parsed.y es undefined
+      // Devuelve un color predeterminado en este caso
+      return 'rgba(169, 169, 169, 1)'; // Gris
+    }
   }
-
+  
+  
+  makeHalfAsOpaque(ctx: any) {
+    const color = this.getLineColor(ctx);
+    // Hace el color semi-opaco dividiendo su valor alfa por la mitad
+    const alpha = color.replace(/[^,]+(?=\))/, '0.5');
+    return alpha;
+  }
+  
+  adjustRadiusBasedOnData(ctx: any) {
+    // Ajusta el radio del punto basado en los datos
+    return 7; // Radio fijo de 7 para todos los puntos
+  }
+  
+  alternatePointStyles(ctx: any) {
+    const index = ctx.dataIndex;
+    return index % 2 === 0 ? 'circle' : 'rect'; // Alterna los estilos de los puntos
+  }
+  
+  
 
   getBpmColor(heartRate: number): string {
     // Definir los rangos de BPM para cada estado
-    const restingHeartRateRange = { min: 60, max: 80 };
+    const lowHeartRateRange = { min: 0, max: 60 };
+    const restingHeartRateRange = { min: 61, max: 80 };
     const warmUpHeartRateRange = { min: 81, max: 100 };
     const fatBurningHeartRateRange = { min: 101, max: 120 };
     const aerobicHeartRateRange = { min: 121, max: 150 };
-    // Puedes agregar más rangos según tus necesidades
-    
+    const highHeartRateRange = { min: 151, max: 200 };
+  
     // Determinar en qué rango se encuentra el valor actual de ritmo cardíaco
-    if (heartRate >= restingHeartRateRange.min && heartRate <= restingHeartRateRange.max) {
-      return 'green'; // Color para el estado de reposo (por ejemplo, verde)
-    } else if (heartRate >= warmUpHeartRateRange.min && heartRate <= warmUpHeartRateRange.max) {
-      return 'orange'; // Color para el estado de calentamiento (por ejemplo, naranja)
+    if (heartRate >= lowHeartRateRange.min && heartRate <= lowHeartRateRange.max) {
+      return '#CBCBCB'; // Color para el estado de bajo ritmo cardíaco
+    } else if (heartRate >= restingHeartRateRange.min && heartRate <= restingHeartRateRange.max) {
+      return '#3EEE08'; // Color para el estado de reposo (por ejemplo, verde)
+    } if (heartRate >= warmUpHeartRateRange.min && heartRate <= warmUpHeartRateRange.max) {
+      return '#E7DC1C '; // Color para el estado de calentamiento (por ejemplo, naranja)
     } else if (heartRate >= fatBurningHeartRateRange.min && heartRate <= fatBurningHeartRateRange.max) {
-      return 'yellow'; // Color para el estado de quema de grasa (por ejemplo, amarillo)
+      return 'orange'; // Color para el estado de quema de grasa (por ejemplo, amarillo)
     } else if (heartRate >= aerobicHeartRateRange.min && heartRate <= aerobicHeartRateRange.max) {
-      return 'blue'; // Color para el estado aeróbico (por ejemplo, azul)
+      return ''; // Color para el estado aeróbico (por ejemplo, azul claro)
+    } else if (heartRate >= highHeartRateRange.min && heartRate <= highHeartRateRange.max) {
+      return 'red'; // Color para el estado de alto ritmo cardíaco
     } else {
-      return 'red'; // Color predeterminado para otros estados (por ejemplo, rojo)
+      return 'purple'; // Color predeterminado para otros estados (por ejemplo, morado)
     }
   }
   
   getHeartRateStatus(heartRate: number): string {
     // Definir los rangos de BPM para cada estado
-    const restingHeartRateRange = { min: 60, max: 80 };
+    const lowHeartRateRange = { min: 0, max: 60 };
+    const restingHeartRateRange = { min: 61, max: 80 };
     const warmUpHeartRateRange = { min: 81, max: 100 };
     const fatBurningHeartRateRange = { min: 101, max: 120 };
     const aerobicHeartRateRange = { min: 121, max: 150 };
-    // Puedes agregar más rangos según tus necesidades
-    
+    const highHeartRateRange = { min: 151, max: 200 };
+  
     // Determinar en qué rango se encuentra el valor actual de ritmo cardíaco
-    if (heartRate >= restingHeartRateRange.min && heartRate <= restingHeartRateRange.max) {
+    if (heartRate >= lowHeartRateRange.min && heartRate <= lowHeartRateRange.max) {
+      return 'Low'; // Estado de bajo ritmo cardíaco
+    } else if (heartRate >= restingHeartRateRange.min && heartRate <= restingHeartRateRange.max) {
       return 'Resting'; // Estado de reposo
     } else if (heartRate >= warmUpHeartRateRange.min && heartRate <= warmUpHeartRateRange.max) {
       return 'Warm Up'; // Estado de calentamiento
@@ -119,9 +206,27 @@ export class HomePage implements OnInit {
       return 'Fat Burning'; // Estado de quema de grasa
     } else if (heartRate >= aerobicHeartRateRange.min && heartRate <= aerobicHeartRateRange.max) {
       return 'Aerobic'; // Estado aeróbico
+    } else if (heartRate >= highHeartRateRange.min && heartRate <= highHeartRateRange.max) {
+      return 'High'; // Estado de alto ritmo cardíaco
     } else {
       return 'Unknown'; // Estado desconocido
     }
+  }
+  
+  updateBpmGradient() {
+    // Calcula la posición del indicador de BPM en porcentaje
+    const positionPercentage = (this.heartRate - 0) / (200 - 0) * 100;
+    this.indicatorPosition = `${positionPercentage}%`;
+  
+    // Actualiza el gradiente de colores de la barra de BPM
+    this.bpmGradient = 'linear-gradient(to right, ' +
+      this.getBpmColor(0) + ' 0%, ' +
+      this.getBpmColor(60) + ' 10%, ' +
+      this.getBpmColor(80) + ' 30%, ' +
+      this.getBpmColor(100) + ' 50%, ' +
+      this.getBpmColor(120) + ' 65%, ' +
+      this.getBpmColor(150) + ' 85%, ' +
+      this.getBpmColor(200) + ' 100%)';
   }
   
 
