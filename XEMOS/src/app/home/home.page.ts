c824bbd6df91cd2247a6783b312ae3093496808a
Roberto Berrendo Eguino <rberrendoe01@informatica.iesvalledeljerteplasencia.es>
2024-04-27 00:05:42 +0200

    style: :lipstick: Update chart value

--- XEMOS/src/app/home/home.page.ts
+++ XEMOS/src/app/home/home.page.ts
@@ -370,7 +370,7 @@ export class HomePage implements OnInit, OnDestroy {
                         text: 'ppb'
                     },
                     min: 0,
-                    max: 1000
+                    max: 100
                 },
                 y1: {
                     type: 'linear',
