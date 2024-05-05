0aaa9f35fdc440a27ca6e6513409d8c10bbfbb06
Alvaromg00 <amorong03@informatica.iesvalledeljerteplasencia.es>
2024-05-05 19:42:56 +0200

    feat: :sparkles: adaptando metodo para llamada de emergencia

--- XEMOS/src/app/emergency/emergency.page.ts
+++ XEMOS/src/app/emergency/emergency.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { AuthService } from '../services/auth.service';
+import { Plugins, Capacitor } from '@capacitor/core';
 
 @Component({
   selector: 'app-emergency',
@@ -23,7 +24,20 @@ export class EmergencyPage implements OnInit {
       (response: any) => {
         const emergencyNumber = response.emergencyNumber;
         if (emergencyNumber) {
-          window.location.href = 'tel:' + emergencyNumber;
+          // Verificar si la plataforma es Android y si los permisos de llamada están disponibles
+          if (Capacitor.isNative && Capacitor.getPlatform() === 'android') {
+            Plugins['Permissions']['requestPermission']('CALL_PHONE').then((result: any) => {
+              if (result.state === 'granted') {
+                // Se concedieron los permisos, iniciar la llamada usando la API de marcación de teléfono de Capacitor
+                Plugins['Device']['openUrl']({ url: 'tel:' + emergencyNumber });
+              } else {
+                console.error('No se otorgaron los permisos para realizar llamadas.');
+              }
+            });
+          } else {
+            // En otras plataformas o si los permisos no son necesarios, iniciar la llamada directamente usando la API de Capacitor
+            Plugins['Device']['openUrl']({ url: 'tel:' + emergencyNumber });
+          }
         } else {
           console.error('Número de emergencia no encontrado para este usuario');
         }
@@ -33,5 +47,7 @@ export class EmergencyPage implements OnInit {
       }
     );
   }
+
+
 
 }
